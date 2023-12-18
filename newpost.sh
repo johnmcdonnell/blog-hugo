81f25d7b713d5bb9131e87a2d91ae4b63c74137f
John McDonnell <john.v.mcdonnell@gmail.com>
2023-12-17 22:01:02 -0800

    Script to create posts

---
+++ newpost.sh
@@ -0,0 +1,11 @@
+
+# Check if $1 is set if not error
+if [ -z "$1" ]
+  then
+    echo "ERROR: Please include post name as the first argument"
+    exit 1
+fi
+
+POSTNAME=$(date +'%Y-%m-%d')_$1.md
+echo Creating $POSTNAME
+hugo new posts/$POSTNAME
